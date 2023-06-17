e30b4deecf25909a4915e39a6adbd840d7605c6c
rocky <1131819936@qq.com>
2023-06-17 10:18:09 +0800

    fix validate aysnc 问题

---
+++ app/api/v1/user.js
@@ -0,0 +1,27 @@
+const Router = require('koa-router')
+const {RegisterValidator} = require('../../validators/validator')
+
+const {
+    User
+} = require('../../models/user')
+
+
+const router = new Router({
+    prefix: '/v1/user'
+})
+
+
+router.post('/register', async (ctx) => {
+    //思维路径
+    //接受参数
+    //email
+    const v = await new RegisterValidator().validate(ctx)
+    const user = {
+        email: v.get('body.email'),
+        password: v.get('body.password2'),
+        nickname: v.get('body.nickname')
+    }
+    await User.create(user)
+})
+
+module.exports = router
